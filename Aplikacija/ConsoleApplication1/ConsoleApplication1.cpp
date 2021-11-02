8667f96ff80fa46bd7317e556c83b8a4f0152193
LuXoN1999 <llovretic99@gmail.com>
2021-11-02 21:39:38 +0100

    3. ZAD

--- Aplikacija/ConsoleApplication1/ConsoleApplication1.cpp
+++ Aplikacija/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,5 +3,9 @@ using namespace std;
 
 int main()
 {
+<<<<<<< HEAD
 	cout << "Pozdrav iz aplikacije od Luka Lovretic. ->0165078883";
+=======
+	cout << "Pozdrav iz aplikacije od Luka Lovretic.";
+>>>>>>> 09b700c (Aplikacija)
 }
